8d8064ff30655eb64a3157882012cce2ba2a3da0
Daniel Kochmański <daniel@turtleware.eu>
2015-09-10 14:48:07 +0200

    tests: enhance
    Signed-off-by: Daniel Kochmański <daniel@turtleware.eu>

--- tests.lisp
+++ tests.lisp
@@ -9,7 +9,8 @@
 (deftest translate.nil-lang.1
     (apply #'values
            (let ((translate:*language* nil))
-             (mapcar #'equal
+             (mapcar #'(lambda (x y) (or (equal x y)
+                                         (list  x y)))
                      (list #t"phrase-1" #t"phrase-2" #t"phrase-3")
                      (list   "phrase-1"   "phrase-2"   "phrase-3"))))
   T T T)
@@ -17,23 +18,27 @@
 (deftest translate.nil-lang.2
     (apply #'values
            (let ((translate:*language* nil))
-           (mapcar #'equal
+           (mapcar #'(lambda (x y) (and (equal x y)
+                                        (list  x y)))
                    '(#t"phrase-1" #t"phrase-2" #t"phrase-3")
                    '(  "phrase-1"   "phrase-2"   "phrase-3"))))
   NIL NIL NIL)
 
-(setf translate:*resolution-time* :load-time)
+(setf translate:*language* nil
+      translate:*resolution-time* :load-time)
 
 (deftest translate.nil-lang.3
     (apply #'values
-           (mapcar #'equal
+           (mapcar #'(lambda (x y) (or (equal x y)
+                                         (list  x y)))
                    (list #t"phrase-1" #t"phrase-2" #t"phrase-3")
                    (list   "phrase-1"   "phrase-2"   "phrase-3")))
   T T T)
 
 (deftest translate.nil-lang.4
     (apply #'values
-           (mapcar #'equal
+           (mapcar #'(lambda (x y) (or (equal x y)
+                                         (list  x y)))
                    '(#t"phrase-1" #t"phrase-2" #t"phrase-3")
                    '(  "phrase-1"   "phrase-2"   "phrase-3")))
   T T T)
@@ -48,7 +53,8 @@
 (deftest translate.en-lang.1
     (apply #'values
            (let ((translate:*language* 'en))
-             (mapcar #'equal
+             (mapcar #'(lambda (x y) (or (equal x y)
+                                         (list  x y)))
                      (list #t"phrase-1"   #t"phrase-2" #t"phrase-3")
                      (list   "Phrase one"  "{phrase-2}" "{phrase-3}"))))
   T T T)
